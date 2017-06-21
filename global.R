6d66fa8c75998f229d53c4502130ddc8beba8621
Eric Anderson <andersone@Erics-MacBook-Pro.local>
2017-06-21 11:52:59 -0400

    updates

--- global.R
+++ global.R
@@ -1,11 +1,10 @@
 globals <- list()
 
-message("envision-index v2")
+message("envision-index v3")
 
 globals$user <- Sys.info()[["user"]]
 
-globals$apps <- sort(list.dirs("/data/shiny-server", 
-                               recursive = FALSE, full.names = FALSE))
+globals$apps <- sort(list.dirs(recursive = FALSE, full.names = FALSE))
 
 globals$metrumGitHub <- "https://raw.githubusercontent.com/metrumresearchgroup"
 

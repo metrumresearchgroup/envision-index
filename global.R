d6447fb0f7ef67ae93224616836c1a46192e4734
Eric Anderson <andersone@Erics-MacBook-Pro.local>
2017-06-21 10:53:33 -0400

    updates

--- global.R
+++ global.R
@@ -3,9 +3,7 @@ globals <- list()
 
 globals$user <- Sys.info()[["user"]]
 
-globals$apps <- sort(list.dirs("/data/shiny-server",
-                               recursive = FALSE,
-                               full.names = FALSE))
+globals$apps <- sort(list.dirs(recursive = FALSE, full.names = FALSE))
 
 globals$metrumGitHub <- "https://raw.githubusercontent.com/metrumresearchgroup"
 

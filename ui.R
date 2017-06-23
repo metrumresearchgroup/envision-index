80fe23a54dfac6bf68642ada1bd693242e9b31de
Eric Anderson <andersone@Erics-MacBook-Pro.local>
2017-06-23 15:29:21 -0400

    updates

--- ui.R
+++ ui.R
@@ -13,47 +13,47 @@ ui <- metrumApp(
             offset = 2,
             uiOutput('appTable')
           )
-        ),
-        div(class = "container-fluid",
-            id = "envision-log-reader",
-            style = "visibility:hidden;",
-            br(),
-            fluidRow(
-              column(
-                width = 1,
-                actionButton(inputId = "showApps", class = "btn-info btn-lg", label = "Back to Apps", icon = icon("step-backward"))
-              ),
-              column(
-                width = 4,
-                uiOutput("logAppName")
-              ),
-              column(
-                width = 2,
-                tags$div(
-                  class = "pull-right",
-                  textInput(inputId = "logDir", "Log Directory", "/var/log/shiny-server", width = "200px")
-                )
-              ),
-              column(
-                width = 3,
-                selectInput("logFile", "Log File", choices = NULL, width = "450px")
-              ),
-              column(
-                width = 2,
-                span(
-                  class = "text-center",
-                  style = "font-size:20px",
-                  checkboxInput("liveStream", "Live Stream", value = TRUE)
-                )
-              )
-            ),
-            fluidRow(
-              column(
-                width = 12,
-                verbatimTextOutput("logContents")
-              )
+        )
+    ),
+    div(class = "container-fluid",
+        id = "envision-log-reader",
+        style = "visibility:hidden;",
+        br(),
+        fluidRow(
+          column(
+            width = 1,
+            actionButton(inputId = "showApps", class = "btn-info btn-lg", label = "Back to Apps", icon = icon("step-backward"))
+          ),
+          column(
+            width = 4,
+            uiOutput("logAppName")
+          ),
+          column(
+            width = 2,
+            tags$div(
+              class = "pull-right",
+              textInput(inputId = "logDir", "Log Directory", "/var/log/shiny-server", width = "200px")
             )
+          ),
+          column(
+            width = 3,
+            selectInput("logFile", "Log File", choices = NULL, width = "450px")
+          ),
+          column(
+            width = 2,
+            span(
+              class = "text-center",
+              style = "font-size:20px",
+              checkboxInput("liveStream", "Live Stream", value = TRUE)
+            )
+          )
+        ),
+        fluidRow(
+          column(
+            width = 12,
+            verbatimTextOutput("logContents")
+          )
         )
     )
   )
-  
+)

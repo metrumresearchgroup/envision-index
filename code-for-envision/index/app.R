d6206e684bc6f1d9befe3cf4428f8c6c45d0ebca
Eric Anderson <andersone@Erics-MacBook-Pro.local>
2017-06-23 09:39:51 -0400

    updates

--- code-for-envision/index/app.R
+++ code-for-envision/index/app.R
@@ -27,10 +27,10 @@ if(runRemoteApp){
     output$shinyServerApps <- renderUI({
       clientURL <- paste0(session$clientData$url_protocol, "//", session$clientData$url_hostname)
       apps <- sort(list.dirs('/data/shiny-server', recursive = FALSE, full.names = FALSE))
-      
+      notApps <- c("index", "rmd")
       indexLinks <- shiny::tags$ul(style = "font-size:18px")
       
-      for(app.i in apps){
+      for(app.i in apps[!(apps %in% notApps)]){
         link.i <- file.path(clientURL, "envision", app.i, "")
         indexLinks <- tagAppendChild(indexLinks,
                                      tags$li(tags$a(href = link.i, target = "_blank", app.i)))

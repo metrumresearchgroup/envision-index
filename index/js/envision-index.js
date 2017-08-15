54f66c0f8d427808ba5b77a884416963cb8f04e2
Eric Anderson <andersone@metrumrg.com>
2017-08-15 19:05:37 +0000

    js updates

--- index/js/envision-index.js
+++ index/js/envision-index.js
@@ -17,7 +17,7 @@ function getAppLink(app) {
 }
 */
 function resizeLogOutput() {
-    $("#logContents").css("max-height", $(window).height() - 240);
+    $("#logContents").css("max-height", $(window).height() - 225);
 }
 
 function scrollBottomLog() {

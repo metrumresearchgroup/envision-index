6c8548a8b60a42772d1869605f295954244b164e
Eric Anderson <andersone@metrumrg.com>
2017-09-13 20:23:37 +0000

    folder reorg

--- misc/development/prep-for-development.R
+++ misc/development/prep-for-development.R
@@ -1,6 +1,6 @@
 
 # app code
-system("rm -rf /data/shiny-server/*")
+system("sudo rm -rf /data/shiny-server/*")
 
 for(to_move.i in c("index", "index.html", "hello", "rmd")){
   
@@ -14,6 +14,6 @@ shiny_server_conf <- "/etc/shiny-server/shiny-server.conf"
 
 system(paste0("sudo chmod 0777 ", shiny_server_conf))
 
-system(paste0("sudo cp /data/envision-index/tests/development/shiny-server.conf ", shiny_server_conf))
+system(paste0("sudo cp /data/envision-index/misc/development/shiny-server.conf ", shiny_server_conf))
 
 system("sudo restart shiny-server")
